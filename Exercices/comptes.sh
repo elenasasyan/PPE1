442ead51f23bb9d4cc69f00dabef0d6f313d63c3
elenasasyan <el.asasyan@gmail.com>
2024-10-21 22:59:17 +0200

    Ajout du fichier comtes.sh

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,14 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-LOCATIONS2016=$(cat ~/Desktop/M1_TAL/S1/ProjetEncadre1/ann/2016/*.ann | grep Location | wc -l)
-echo "Lieux en 2016:"
-echo "$LOCATIONS2016"
-
-LOCATIONS2017=$(cat ~/Desktop/M1_TAL/S1/ProjetEncadre1/ann/2017/*.ann | grep Location | wc -l)
-echo "Lieux en 2017:"
-echo "$LOCATIONS2017"
-
-LOCATIONS2018=$(cat ~/Desktop/M1_TAL/S1/ProjetEncadre1/ann/2018/*.ann | grep Location | wc -l)
-echo "Lieux en 2018:"
-echo "$LOCATIONS2018"
-
+for ANNEE in 2016 2017 2018
+do
+    LIEUX=$(cat ../../ann/$ANNEE/*.ann | grep Location | wc -l)
+    echo -e "Lieux en $ANNEE:\n$LIEUX"
+done
